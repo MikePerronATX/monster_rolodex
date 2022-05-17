5627cda013ac7bd6a6b85d3223f8db2ba5810bf3
Michael Perron <mikeperron.atx@gmail.com>
2022-05-16 20:31:25 -0500

    filtering list

--- src/App.js
+++ src/App.js
@@ -1,39 +1,60 @@
 import { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
       monsters: [],
+      searchField: ''
     };
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users) => this.setState(() => {
-        return {monsters: users}
-      },
-      () => {
-        console.log(this.state);
-      }
-      ));
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
-  render () {
+  render() {
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
-    <div className='App'>
-      <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
-        console.log(event)
-      }}/>
-        {this.state.monsters.map((monster) => {
-            return (<div key={monster.id}>
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchField = event.target.value.toLocaleLowerCase();
+            
+
+            this.setState(() => {
+              return { searchField};
+            });
+          }}
+        />
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
               <h1>{monster.name}</h1>
-              </div>
-            );
+            </div>
+          );
         })}
       </div>
     );
